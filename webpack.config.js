f8af74f219df8815b859e1059ab22611f826ee98
sym <849228735@qq.com>
2018-03-09 03:53:35 +0800

    1. add babel, 2. uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: ["babel-polyfill", "./src/index.tsx"],
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
@@ -16,10 +16,16 @@ module.exports = {
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader",
+				options: {
+					useBabel: true
+				}
+			},
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
         ]
     },
 
